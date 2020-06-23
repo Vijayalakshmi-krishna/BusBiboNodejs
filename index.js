803dfdcb4b86ee51244492c04edba3c4ad8d6546
vijayalakshmi-krishna <61419132+vijayalakshmi-krishna@users.noreply.github.com>
2020-06-23 23:26:06 +0530

    revert

--- index.js
+++ index.js
@@ -14,16 +14,17 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.options("/*", function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.sendStatus(200);
+});
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://bus-zone.herokuapp.com");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
     next();
-  });
+});
 
 app.post("/login", function (req, res) {
     
